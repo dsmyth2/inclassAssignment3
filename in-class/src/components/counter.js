08bdbe4f8f9f7ffe920e21754d3843cedcc4e851
dsmyth2 <97994342+dsmyth2@users.noreply.github.com>
2023-11-16 14:05:55 -0500

    styling edits

--- in-class/src/components/counter.js
+++ in-class/src/components/counter.js
@@ -11,21 +11,22 @@ const counter = () => {
     setCount(count + 1);
   }
   return (
-    <body className="bg-dark min-vh-100 d-flex align-items-center">
-      <div className="body-wrapper d-flex flex-column align-items-center min-vw-100">
-        <p className="border border-rounded py-1 px-4 text-light counter h1 mb-5">
+    <body className="bg-secondary min-vh-100 d-flex align-items-center">
+      <div className="body-wrapper bg-light d-flex flex-column align-items-center m-auto p-4 border border-2 border-primary rounded">
+        <h1 className="h1 text-dark py-2">Counter App</h1>
+        <p className="border border-primary border-2 rounded py-1 px-4 text-dark counter h1 mb-5">
           {count}
         </p>
         <div className="buttons ">
           <button
-            className="btn btn-primary btn-lg px-4 mx-5"
+            className="btn btn-primary btn-lg px-4 mx-5 fs-3"
             name="decrement-button"
             onClick={decrement}
           >
             -
           </button>
           <button
-            className="btn btn-primary btn-lg px-4 mx-5"
+            className="btn btn-primary btn-lg px-4 mx-5 fs-3"
             name="increment-button"
             onClick={increment}
           >
